73eec551fd1f3d1d61894f971555826e788c113d
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-08 05:48:15 +0200

    Add project files.

---
+++ IntroductionToClassesExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToClassesExercise
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine($"Hi {to}, I am {Name}");
+        }
+
+        public Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Person person = new Person();
+            var p = person.Parse("John");
+            person.Name = "John";
+            person.Introduce("Abe");
+        }
+    }
+}
